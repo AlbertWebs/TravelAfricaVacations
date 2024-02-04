f39c4d0814042804540922832230f955a17c4ed3
AlbertWebs <albertmuhatia@gmail.com>
2024-02-04 15:36:12 +0300

    Update Images for Sample Tours

--- resources/views/front/kenya.blade.php
+++ resources/views/front/kenya.blade.php
@@ -45,7 +45,10 @@
                </div>
                <div class="content">
                   <h4 style="height:70px !important;"><a href="{{url('/')}}/sample-safaris/{{$Ex->slung}}">{{$Ex->title}}</a></h4>
-
+                  <div class="best-plan-meta">
+                     {{-- <span class="duration"><i class="bi bi-clock"></i>{{$Ex->duration}}</span> --}}
+                     <span class="rating"><i class="bi bi-geo-alt"></i>{{$Ex->location}}</span>
+                  </div>
                   <a href="{{url('/')}}/sample-safaris/{{$Ex->slung}}" class="eg-btn btn--primary-outline btn--md">Explore Itinery</a>
                </div>
             </div>
